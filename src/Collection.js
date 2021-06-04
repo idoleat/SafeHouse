8bb2f051c2717c9aea0490f6985a207525deffeb
C.W. Chien <dppss92132@gmail.com>
2021-06-04 23:55:20 +0800

    Make SQLite integration to another branch
    Delete the related files. NoSQLite version will be the main version. 
    It's better to make it an option to choose from, since users are 
    required to have technical skills to operate SQLite to use SQLite 
    version.

--- src/Collection.js
+++ src/Collection.js
@@ -1,19 +1,2 @@
 let Parameters = new URLSearchParams(document.location.search);
 let CollectionName = Parameters.get('cl'); // Only the first occurence will be returned
-
-SQLiteDBWorker.get().then( async (worker) => {
-  console.log('Async querying : select * from mytable .... Result = ' + JSON.stringify(
-    await worker.db.query(`select * from mytable`)
-  ));
-});
-
-//SELECT
-//  name,
-//  tags,
-//  content,
-//FROM
-//  items
-//WHERE
-//  name=CollectoinsName
-
-// DOM purify: https://github.com/cure53/DOMPurify
