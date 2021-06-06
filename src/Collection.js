01cf3cbb858745273081d1f73b01f594e1e3bfff
C.W. Chien <dppss92132@gmail.com>
2021-06-06 22:11:20 +0800

    Be able to load collection json

--- src/Collection.js
+++ src/Collection.js
@@ -1,2 +1,17 @@
 let Parameters = new URLSearchParams(document.location.search);
 let CollectionName = Parameters.get('cl'); // Only the first occurence will be returned
+let Collection;
+
+function GetCollection_json(callback){
+  fetch('./items/' + CollectionName + '.json').then(function(response) {
+    return response.json();
+  }).then(function(json){
+    Collection = json;
+    if(callback != undefined) callback();
+  }).catch(function(error){
+    alert('Lost the way to your save house :(( \n' + error); // TEMP: need to display error directly in the web instead with a nice way like discord.
+  });
+}
+
+GetCollection_json();
+window.onload = () => document.getElementById('cl_name').innerHTML = CollectionName
