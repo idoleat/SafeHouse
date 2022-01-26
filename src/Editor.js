f43327841e8e07d358f862c426c18e32b2dd98a2
C.W. Chien <dppss92132@gmail.com>
2022-01-26 16:53:47 +0800

    Fix scrollable popup
    By disable body element overflow

--- src/Editor.js
+++ src/Editor.js
@@ -2,6 +2,7 @@ function OpenEditor() {
   document.getElementById("editor").className = "Editor";
   document.getElementById("editor-window").className = "Editor-window";
   document.getElementById("editor-close-btn").className = "Editor-close-btn";
+  document.body.classList.add("Disable-scroll");
 }
 
 function CloseEditor() {
@@ -10,4 +11,5 @@ function CloseEditor() {
     "Editor-window Editor-disabled";
   document.getElementById("editor-close-btn").className =
     "Editor-close-btn Editor-disabled";
+  document.body.classList.remove("Disable-scroll");
 }
