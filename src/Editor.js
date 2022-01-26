f91ff0fe04774ee0977b78659e21f71b8fc01e38
C.W. Chien <dppss92132@gmail.com>
2022-01-26 20:25:00 +0800

    Add feature to cluse editor when background clicked
    Make a fullscreen overlay under editor as a huge close button

--- src/Editor.js
+++ src/Editor.js
@@ -1,7 +1,9 @@
 function OpenEditor() {
   document.getElementById("editor").className = "Editor";
+  //document.getElementById("editor").setAttribute("onClick", "CloseEditor()");
   document.getElementById("editor-window").className = "Editor-window";
   document.getElementById("editor-close-btn").className = "Editor-close-btn";
+  document.getElementById("editor-bg").className = "Editor-bg";
   document.body.classList.add("Disable-scroll");
 }
 
@@ -11,5 +13,6 @@ function CloseEditor() {
     "Editor-window Editor-disabled";
   document.getElementById("editor-close-btn").className =
     "Editor-close-btn Editor-disabled";
+  document.getElementById("editor-bg").className = "Editor-bg Editor-disabled";
   document.body.classList.remove("Disable-scroll");
 }
